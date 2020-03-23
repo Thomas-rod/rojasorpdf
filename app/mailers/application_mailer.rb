d7016b70f2de4ef4bee3e16f48953fe6a44f77d5
thomas <rodiert17@gmail.com>
2020-03-23 14:35:13 +0100

    few changes on pdf

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'rojasorfrance@gmail.com'
+  default from: 'rojasorfrance@gmail.com', cc: 'rojasorfrance@gmail.com'
   layout 'mailer'
 end
